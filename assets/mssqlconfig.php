cee421c8b7d2b311921cf88e57588ec3997bbd51
Mashy <mbanga.anele@gmail.com>
2022-05-02 15:34:17 +0200

    A Netlify Deployment

---
+++ assets/mssqlconfig.php
@@ -0,0 +1,6 @@
+<?php
+const DB_HOST = '(local)\sqlexpress';
+const DB_NAME = 'estore';
+const DB_USER = 'sa';
+const DB_PASS = 'usioloeso123';
+?>
---
+++ assets/mysqlconfig.php
@@ -0,0 +1,7 @@
+<?php
+const DB_HOST = 'localhost';
+const DB_NAME = 'bvad3products';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_KEY = 'mashy';
+?>
